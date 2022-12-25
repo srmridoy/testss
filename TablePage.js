9fc7db7f84ac76c9335ccefafc914487838d2274
srmridoy <55218368+srmridoy@users.noreply.github.com>
2022-12-25 06:57:58 +0600

    Update TablePage.js

--- TablePage.js
+++ TablePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 
 import { PageTitle } from '../../layout-components';
-import { Grid } from '@material-ui/core';
 
 export default function ElementsAvatars() {
   return (
